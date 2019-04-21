b21f6dcd6b07d1ccccefadc509d70bb200604fcd
Gabe Bravo <gabecbravo@gmail.com>
2019-04-21 17:14:43 -0400

    Put back basic css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css'
 
 // import context and reducer
 import { GlobalContext, globalReducer } from './context';
